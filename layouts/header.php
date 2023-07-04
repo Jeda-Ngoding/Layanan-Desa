ecc082b784cc37a0b9df6640c7d0cb07a24630be
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2023-07-04 20:21:59 +0700

    link

--- layouts/header.php
+++ layouts/header.php
@@ -22,7 +22,7 @@
     <main>
         <div class="container">
             <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                     <img width="50"
                         src="/layanan_desa/assets/logo_kabupaten_madiun.gif"
                         alt="">
