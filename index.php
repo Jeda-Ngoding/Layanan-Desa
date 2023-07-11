ae8e51e6e8d582d04690b263fcb0702f096c7d54
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2023-07-11 22:00:34 +0700

    tak bisa ber word word

--- index.php
+++ index.php
@@ -22,24 +22,24 @@ error_reporting(E_ALL);
                         <img src="assets/image-1.jpeg"
                             class="d-block w-100 h-100 rounded" alt="...">
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
+                            <h5>Pelantikan & Pengambilan Sumpah Jabatan</h5>
+                            <p>Pelantikan & Pengambilan Sumpah Jabatan Kepala Dusun Punden Sebelum Menjalani Masa Tugas.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
                         <img src="assets/image-2.jpeg"
                             class="d-block w-100 h-100 rounded" alt="...">
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
+                            <h5>Kegiatan Sosialisasi Hidup Sehat</h5>
+                            <p>Kegiatan Positif Untuk Meningkatkan Wawasan Masyarakat Akan Pentingnya Menjaga Pola Hidup Sehat.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
                         <img src="assets/image-3.jpeg"
                             class="d-block w-100 h-100 rounded" alt="...">
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>Third slide label</h5>
-                            <p>Some representative placeholder content for the third slide.</p>
+                            <h5>Jum'at Sehat</h5>
+                            <p>Kegiatan Olahraga Rutin Sekecamatan Yang Dilaksanakan Tiap Akhir Bulan.</p>
                         </div>
                     </div>
                 </div>
